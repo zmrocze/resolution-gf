f392cbb29379bf4e3a86e1dc2296fe80959e74ac
zmrocze <karolochmanmilarski@gmail.com>
2022-10-12 20:41:50 +0200

    Before hkt fight.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,18 +3,18 @@ import cats.Functor
 @main def hello: Unit = 
   println("Hello world!")
 
-sealed trait GFFormula[A] extends Functor[GFFormula]:
-  override def map[A, B](fa: GFFormula[A])(f: A => B): GFFormula[B] = fa match 
-    case (Not(sub)) => Not(lift(f)(sub))
-    case (And(left, right)) => And(lift(f)(left), lift(f)(right))
-    case (Or(left, right)) => Or(lift(f)(left), lift(f)(right))
-    case Atom(atom) => Atom(f(atom))
-    case Forall(guard, sub) => Forall(f(guard), lift(f)(sub))
-    case Exist(guard, sub) => Exist(f(guard), lift(f)(sub))
+sealed trait GFFormula[A, X]
+  // override def map[A[_], B[_], X, Y](fa: GFFormula[A[_], X])(f: A[X] => B[Y]): GFFormula[B] = fa match 
+  //   case (Not(sub)) => Not(lift(f)(sub))
+  //   case (And(left, right)) => And(lift(f)(left), lift(f)(right))
+  //   case (Or(left, right)) => Or(lift(f)(left), lift(f)(right))
+  //   case Atom(atom) => Atom(f(atom))
+  //   case Forall(guard, sub) => Forall(f(guard), lift(f)(sub))
+  //   case Exist(guard, sub) => Exist(f(guard), lift(f)(sub))
 
-case class Not[A](sub : GFFormula[A]) extends GFFormula[A]
-case class And[A](left : GFFormula[A], right : GFFormula[A]) extends GFFormula[A]
-case class Or[A](left : GFFormula[A], right : GFFormula[A]) extends GFFormula[A]
-case class Atom[A](atom : A) extends GFFormula[A]
-case class Forall[A](guard : A, sub : GFFormula[A]) extends GFFormula[A]
-case class Exist[A](guard : A, sub : GFFormula[A]) extends GFFormula[A]
+case class Not[A[_], X](sub : GFFormula[A[_], X]) extends GFFormula[A[_], X]
+case class And[A[_], X](left : GFFormula[A[_], X], right : GFFormula[A[_], X]) extends GFFormula[A[_], X]
+case class Or[A[_], X](left : GFFormula[A[_], X], right : GFFormula[A[_], X]) extends GFFormula[A[_], X]
+case class Atom[A[_], X](atom : A[X]) extends GFFormula[A[_], X]
+case class Forall[A[_], X](variable : X, guard : A[X], sub : GFFormula[A[_], X]) extends GFFormula[A[_], X]
+case class Exist[A[_], X](variable : X, guard : A[X], sub : GFFormula[A[_], X]) extends GFFormula[A[_], X]
--- src/main/scala/NNF.scala
+++ src/main/scala/NNF.scala
@@ -1,28 +1,29 @@
 
-sealed trait NNFedGFFormula[T, A] {
+sealed abstract class NNFedGFFormula[T, A, X] {
   val tag : T
 }
 
-case class NNFLiteral[T, A](tag : T, negation : Boolean, atom : A) extends NNFedGFFormula[T, A]
-case class NNFNot[T, A](tag : T, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
-case class NNFAnd[T, A](tag : T, left : NNFedGFFormula[T, A], right : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
-case class NNFOr[T, A](tag : T, left : NNFedGFFormula[T, A], right : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
-case class NNFAtom[T, A](tag : T, atom : A) extends NNFedGFFormula[T, A]
-case class NNFForall[T, A](tag : T, guard : A, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
-case class NNFExist[T, A](tag : T, guard : A, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
+case class NNFLiteral[T, A, X](tag : T, negation : Boolean, atom : A) extends NNFedGFFormula[T, A, X]
+case class NNFNot[T, A, X](tag : T, sub : NNFedGFFormula[T, A, X]) extends NNFedGFFormula[T, A, X]
+case class NNFAnd[T, A, X](tag : T, left : NNFedGFFormula[T, A, X], right : NNFedGFFormula[T, A, X]) extends NNFedGFFormula[T, A, X]
+case class NNFOr[T, A, X](tag : T, left : NNFedGFFormula[T, A, X], right : NNFedGFFormula[T, A, X]) extends NNFedGFFormula[T, A, X]
+case class NNFAtom[T, A[_], X](tag : T, atom : A[X]) extends NNFedGFFormula[T, A[_], X]
+case class NNFForall[T, A[_], X](tag : T, variable : X, guard : A[X], sub : NNFedGFFormula[T, A[_], X]) extends NNFedGFFormula[T, A[_], X]
+case class NNFExist[T, A[_], X](tag : T, variable : X, guard : A[X], sub : NNFedGFFormula[T, A[_], X]) extends NNFedGFFormula[T, A[_], X]
 
 
-def nnf[A](phi : GFFormula[A]): NNFedGFFormula[Unit, A] = phi match {
-  case Not (Atom(a)) => NNFLiteral((), false, a)
-  case Not (Not(g)) => nnf(g)
-  case Not (And(g, h)) => NNFOr((), nnf (Not(g)), nnf (Not(h)))
-  case Not (Or(g, h)) => NNFAnd((), nnf (Not(g)), nnf (Not(h)))
-  case And(g, h) => NNFAnd((), (nnf(g)), (nnf(h)))
-  case Or(g, h) => NNFOr((), nnf(g), nnf(h))
-  case Atom(a) => NNFLiteral((), true, a)
-  case Not (Forall(p, g)) => NNFExist ((), p, (nnf (Not(g))))
-  case Not (Exist(p, g)) => NNFForall((), p, (nnf (Not(g))))
-  case Exist(p, g) => NNFExist((), p, nnf(g))
-  case Forall(p, g) => NNFForall ((), p, nnf(g))
-}
+// def nnf[A[_], X](phi : GFFormula[A, X]): NNFedGFFormula[Unit, A, X] = phi match {
+//   case Not (Atom(a)) => NNFLiteral((), false, a)
+//   case Not (Not(g)) => nnf(g)
+//   case Not (And(g, h)) => NNFOr((), nnf (Not(g)), nnf (Not(h)))
+//   case Not (Or(g, h)) => NNFAnd((), nnf (Not(g)), nnf (Not(h)))
+//   case And(g, h) => NNFAnd((), (nnf(g)), (nnf(h)))
+//   case Or(g, h) => NNFOr((), nnf(g), nnf(h))
+//   case Atom(a) => NNFLiteral((), true, a)
+//   case Not (Forall(variable, p, g)) => NNFExist ((), variable, p, (nnf (Not(g))))
+//   case Not (Exist(variable, p, g)) => NNFForall((), variable, p, (nnf (Not(g))))
+//   case Exist(variable, p, g) => NNFExist((), variable, p, nnf(g))
+//   case Forall(variable, p, g) => NNFForall ((), variable, p, nnf(g))
+// }
 
+case class AtomicFormula[R, X](relation : R, varlist : List[X])
