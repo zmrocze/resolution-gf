37a8420989dd4d808f4af4023fe2af0d420c6fd0
zmrocze <karolochmanmilarski@gmail.com>
2022-10-12 19:47:23 +0200

    Add tag to NNF.

--- src/main/scala/NNF.scala
+++ src/main/scala/NNF.scala
@@ -1,26 +1,28 @@
 
+sealed trait NNFedGFFormula[T, A] {
+  val tag : T
+}
 
-sealed trait NNFedGFFormula[A]
-case class NNFLiteral[A](negation : Boolean, atom : A) extends NNFedGFFormula[A]
-case class NNFNot[A](sub : NNFedGFFormula[A]) extends NNFedGFFormula[A]
-case class NNFAnd[A](left : NNFedGFFormula[A], right : NNFedGFFormula[A]) extends NNFedGFFormula[A]
-case class NNFOr[A](left : NNFedGFFormula[A], right : NNFedGFFormula[A]) extends NNFedGFFormula[A]
-case class NNFAtom[A](atom : A) extends NNFedGFFormula[A]
-case class NNFForall[A](guard : A, sub : NNFedGFFormula[A]) extends NNFedGFFormula[A]
-case class NNFExist[A](guard : A, sub : NNFedGFFormula[A]) extends NNFedGFFormula[A]
+case class NNFLiteral[T, A](tag : T, negation : Boolean, atom : A) extends NNFedGFFormula[T, A]
+case class NNFNot[T, A](tag : T, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
+case class NNFAnd[T, A](tag : T, left : NNFedGFFormula[T, A], right : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
+case class NNFOr[T, A](tag : T, left : NNFedGFFormula[T, A], right : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
+case class NNFAtom[T, A](tag : T, atom : A) extends NNFedGFFormula[T, A]
+case class NNFForall[T, A](tag : T, guard : A, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
+case class NNFExist[T, A](tag : T, guard : A, sub : NNFedGFFormula[T, A]) extends NNFedGFFormula[T, A]
 
 
-def nnf[A](phi : GFFormula[A]): NNFedGFFormula[A] = phi match {
-  case Not (Atom(a)) => NNFLiteral(false, a)
+def nnf[A](phi : GFFormula[A]): NNFedGFFormula[Unit, A] = phi match {
+  case Not (Atom(a)) => NNFLiteral((), false, a)
   case Not (Not(g)) => nnf(g)
-  case Not (And(g, h)) => NNFOr(nnf (Not(g)), nnf (Not(h)))
-  case Not (Or(g, h)) => NNFAnd(nnf (Not(g)), nnf (Not(h)))
-  case And(g, h) => NNFAnd((nnf(g)), (nnf(h)))
-  case Or(g, h) => NNFOr(nnf(g), nnf(h))
-  case Atom(a) => NNFLiteral(true, a)
-  case Not (Forall(p, g)) => NNFExist (p, (nnf (Not(g))))
-  case Not (Exist(p, g)) => NNFForall(p, (nnf (Not(g))))
-  case Exist(p, g) => NNFExist(p, nnf(g))
-  case Forall(p, g) => NNFForall (p, nnf(g))
+  case Not (And(g, h)) => NNFOr((), nnf (Not(g)), nnf (Not(h)))
+  case Not (Or(g, h)) => NNFAnd((), nnf (Not(g)), nnf (Not(h)))
+  case And(g, h) => NNFAnd((), (nnf(g)), (nnf(h)))
+  case Or(g, h) => NNFOr((), nnf(g), nnf(h))
+  case Atom(a) => NNFLiteral((), true, a)
+  case Not (Forall(p, g)) => NNFExist ((), p, (nnf (Not(g))))
+  case Not (Exist(p, g)) => NNFForall((), p, (nnf (Not(g))))
+  case Exist(p, g) => NNFExist((), p, nnf(g))
+  case Forall(p, g) => NNFForall ((), p, nnf(g))
 }
 
